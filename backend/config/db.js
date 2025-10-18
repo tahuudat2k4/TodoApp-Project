21d4c850c586b2d857be559c67141c648baed2d9
Tạ Hữu Đạt <tahuudat16032004@gmail.com>
2025-10-19 00:24:01 +0700

    Add MONGO_URL_CLOUD

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 const colors = require('colors');
 
 const connectDB = async ()=>{
+    const MONGO_URI = process.env.DEV_MODE === 'development' ? process.env.MONGO_URL_LOCAL : process.env.MONGO_URL_CLOUD;
+
     try {
-        const conn= await mongoose.connect(process.env.MONGO_URL_LOCAL)
+        const conn= await mongoose.connect(MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log(`Connected to Mongodb ${mongoose.connection.host}`.bgGreen.white)
     } catch (error) {
         console.log(`Mongodb Error: ${error}`.bgRed.white)
