03e644752d781a35bd807269fc295f6c889a735b
Tạ Hữu Đạt <tahuudat16032004@gmail.com>
2025-10-19 02:28:21 +0700

    Check lai axios

--- client/src/Services/axiosInstance.js
+++ client/src/Services/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api/v1";
 const instance = axios.create({
     baseURL: API_URL,
 });
